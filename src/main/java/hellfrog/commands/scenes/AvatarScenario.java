276a2aeff5db3e1268b560b2c292faaa222d95d0
Scratch Paws <scratchpaws@eml.cc>
2020-12-27 03:37:19 +0300

    Avatar scenario: avatars download logic moved to user utils

--- src/main/java/hellfrog/commands/scenes/AvatarScenario.java
+++ src/main/java/hellfrog/commands/scenes/AvatarScenario.java
@@ -1,17 +1,11 @@
 package hellfrog.commands.scenes;
 
-import hellfrog.common.BroadCast;
-import hellfrog.common.CommonConstants;
 import hellfrog.common.CommonUtils;
-import hellfrog.common.SimpleHttpClient;
+import hellfrog.common.DiscordImage;
+import hellfrog.common.OperationException;
+import hellfrog.common.UserUtils;
 import hellfrog.core.ServerSideResolver;
 import hellfrog.settings.SettingsController;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.util.EntityUtils;
-import org.javacord.api.entity.Icon;
 import org.javacord.api.entity.channel.PrivateChannel;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.message.MessageBuilder;
@@ -23,9 +17,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
@@ -79,68 +70,25 @@ public class AvatarScenario
                 target = mayBeTarget.get();
                 nickName = server.getDisplayName(target);
             }
-            final Icon avatar = target.getAvatar();
-            final String fileName = avatar.getUrl().getFile();
-            final String[] nameParts = fileName.split("\\.");
-            final String ext = nameParts.length > 0 ? nameParts[nameParts.length - 1] : "png";
-            URI largeSizeURI;
+
             try {
-                largeSizeURI = new URI(avatar.getUrl() + "?size=1024");
-            } catch (URISyntaxException err) {
-                showErrorMessage("Unable to fetch user avatar", event);
-                String errMsg = String.format("Unable to create URI from \"%s\", (%s): %s", avatar.getUrl() + "?size=1024",
-                        this.getClass().getSimpleName(), err.getMessage());
-                BroadCast.sendServiceMessage(errMsg);
-                return;
-            }
-            final SimpleHttpClient client = SettingsController.getInstance().getHttpClientsPool().borrowClient();
-            try {
-                HttpGet imageRequest = new HttpGet(largeSizeURI);
-                byte[] imageFile;
-                try (CloseableHttpResponse httpResponse = client.execute(imageRequest)) {
-                    final int statusCode = httpResponse.getStatusLine().getStatusCode();
-                    if (statusCode != HttpStatus.SC_OK) {
-                        showErrorMessage("Unable to fetch user avatar: HTTP " + statusCode, event);
-                        return;
-                    }
-                    final HttpEntity httpEntity = httpResponse.getEntity();
-                    try {
-                        imageFile = EntityUtils.toByteArray(httpEntity);
-                    } finally {
-                        EntityUtils.consume(httpEntity);
-                    }
-                } catch (IOException err) {
-                    String errMsg = String.format("Unable to download user avatar from Discord CDN: %s",
-                            err.getMessage());
-                    showErrorMessage(errMsg, event);
-                    return;
-                }
-                if (imageFile == null || imageFile.length == 0) {
-                    showErrorMessage("Unable to download user avatar from Discord CDN, received empty image",
-                            event);
-                    return;
-                }
-                if (imageFile.length > CommonConstants.MAX_FILE_SIZE) {
-                    new MessageBuilder().setEmbed(
-                            new EmbedBuilder()
-                                    .setDescription(target.getMentionTag())
-                                    .setColor(Color.ORANGE)
-                                    .setTitle("[" + nickName + "](" + largeSizeURI.toString() + ")")
-                                    .setImage(largeSizeURI.toString())
-                                    .setTimestampToNow()
-                    ).send(event.getChannel());
+                DiscordImage avatar = UserUtils.getAvatar(target);
+                EmbedBuilder embedBuilder = new EmbedBuilder()
+                        .setDescription(target.getMentionTag())
+                        .setColor(Color.ORANGE)
+                        .setTimestampToNow();
+                if (avatar.getImageURI() != null) {
+                    embedBuilder.setTitle("[" + nickName + "](" + avatar.getImageURI().toString() + ")");
                 } else {
-                    new MessageBuilder().setEmbed(
-                            new EmbedBuilder()
-                                    .setDescription(target.getMentionTag())
-                                    .setColor(Color.ORANGE)
-                                    .setTitle(nickName)
-                                    .setImage(imageFile, ext)
-                                    .setTimestampToNow()
-                    ).send(event.getChannel());
+                    embedBuilder.setTitle(nickName);
                 }
-            } finally {
-                SettingsController.getInstance().getHttpClientsPool().returnClient(client);
+                avatar.setImage(embedBuilder);
+                new MessageBuilder()
+                        .setEmbed(embedBuilder)
+                        .send(event.getChannel());
+            } catch (OperationException err) {
+                showErrorMessage(err.getUserMessage(), event);
+                log.error(err.getServiceMessage());
             }
         }));
     }
