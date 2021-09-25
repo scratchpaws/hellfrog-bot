154d45cf1e40562c948607e26c2f0228422e8bb9
Scratch Paws <scratchpaws@eml.cc>
2021-09-26 00:57:40 +0300

    (legacy) Added initial roll value

--- src/main/java/hellfrog/reacts/dice/InitialModifierType.java
+++ src/main/java/hellfrog/reacts/dice/InitialModifierType.java
@@ -1,2 +1,14 @@
-package hellfrog.reacts.dice;public enum InitialModifierType {
+package hellfrog.reacts.dice;
+
+public enum InitialModifierType {
+    NONE(false),
+    SUB_SUM(true),
+    SUB_VAL(false),
+    ADD_SUM(true),
+    ADD_VAL(false);
+
+    public final boolean summary;
+    InitialModifierType(final boolean summary) {
+        this.summary = summary;
+    }
 }
